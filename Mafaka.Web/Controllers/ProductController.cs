ca1077d9a0c5e09cdd0b219c6a3937af9ffafaff
Aleksey <Glodovskiy>
2023-01-05 10:02:07 +0200

    Updated ProductController and product views

--- Mafaka.Web/Controllers/ProductController.cs
+++ Mafaka.Web/Controllers/ProductController.cs
@@ -2,6 +2,7 @@
 using Mafaka.Web.Services.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using System.Reflection;
 
 namespace Mafaka.Web.Controllers
 {
@@ -26,6 +27,54 @@ namespace Mafaka.Web.Controllers
         public async Task<IActionResult> ProductCreate()
         {
             return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto productDto)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await _productService.CreateProductAsync<ResponseDto>(productDto);
+                if(response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(productDto);
+        }
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            var response = await _productService.GetProductByIdAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                var model = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(model);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductEdit(ProductDto productDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productService.UpdateProductAsync<ResponseDto>(productDto);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+                }
+            }
+            return View(productDto);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+            var response = await _productService.DeleteProductAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                return RedirectToAction(nameof(ProductIndex));
+            }
+            return BadRequest();
         }
     }
 }
