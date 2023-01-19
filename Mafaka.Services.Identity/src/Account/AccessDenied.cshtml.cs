805ef21f024879e579f4b2bd9935447fcb729f98
Glodovskij <glodovskii@gmail.com>
2023-01-19 09:53:32 +0200

    Added Identity project with files

---
+++ Mafaka.Services.Identity/src/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace mafaka_shop.Pages.Account;
+
+public class AccessDeniedModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
---
+++ Mafaka.Services.Identity/src/Account/Logout/LogoutOptions.cs
@@ -0,0 +1,8 @@
+
+namespace mafaka_shop.Pages.Logout;
+
+public class LogoutOptions
+{
+    public static bool ShowLogoutPrompt = true;
+    public static bool AutomaticRedirectAfterSignOut = false;
+}
